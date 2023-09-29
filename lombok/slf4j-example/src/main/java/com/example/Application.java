69211118f6cf74beabef742b778b779a37d05b71
zaheerahmed8845 <zaheerahmed8845@gmail.com>
2023-09-29 10:20:25 +0530

    lombok initial commit

---
+++ lombok/slf4j-example/src/main/java/com/example/Application.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+
+    public static void main(String[] args) {
+        log.info("Slf4j lombok tutorial");
+    }
+}
